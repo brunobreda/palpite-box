6a342376bc296dc01b500fd3eaa0de185359e75f
Bruno Breda Rossi <brunorossi.1@hotmail.com>
2020-08-28 15:45:08 -0300

    fix

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -2,8 +2,13 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
-export default async (req, res) => {
+const fromBase64 = value => {
+  const buff = Buffer.from(value, 'base64');
+  return buff.toString('ascii')
+}
 
+export default async (req, res) => {
+  console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
   try {
     //await doc.useServiceAccountAuth(credentials)//dado que eu tenho a planilha do doc, ele autentica com o creddentials importado no inicio desse arquivo js
     await doc.useServiceAccountAuth({
